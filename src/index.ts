cee25c3c7bfc03841b47513e527e0aa84fac3e7d
ayush6999 <ayushjaiswal6999@gmail.com>
2024-03-30 20:58:53 +0530

    setup bakend

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// app.ts
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from server!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
