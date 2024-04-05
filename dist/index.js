745168ac10951d248ab95f95f4b9261756f58709
ayush6999 <ayushjaiswal6999@gmail.com>
2024-04-05 15:54:27 +0530

    Ws to SocketIo and middleware for auth

--- dist/index.js
+++ dist/index.js
@@ -3,73 +3,96 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// Server side - Express app
 const express_1 = __importDefault(require("express"));
 const http_1 = __importDefault(require("http"));
-const ws_1 = __importDefault(require("ws"));
 const child_process_1 = require("child_process");
-const options_1 = require("./rtmpOptions/options");
+const socket_io_1 = require("socket.io");
+// import login from './routes/auth/login';
+// import signup from './routes/auth/signup';
 const app = (0, express_1.default)();
 const port = 3000;
 const server = http_1.default.createServer(app);
-const wss = new ws_1.default.Server({ server });
-// @ts-ignore
-const ffmpegProcess = (0, child_process_1.spawn)('ffmpeg', options_1.options);
-// @ts-ignore
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "http://localhost:3001",
+        methods: ["GET", "POST"]
+    }
+});
+const options = [
+    '-i',
+    '-',
+    '-c:v', 'libx264',
+    '-preset', 'ultrafast',
+    '-tune', 'zerolatency',
+    '-r', '25',
+    '-g', '50', // This should be multiplied by 2 for keyframe interval (GOP size)
+    '-keyint_min', '25',
+    '-crf', '25',
+    '-pix_fmt', 'yuv420p',
+    '-sc_threshold', '0',
+    '-profile:v', 'main',
+    '-level', '3.1',
+    '-c:a', 'aac',
+    '-b:a', '128k',
+    '-ar', '32000', // Adjust the audio sample rate as needed
+    '-f', 'flv',
+    'rtmp://a.rtmp.youtube.com/live2/w0yv-1v24-4rp2-c3sa-3ze9',
+];
+const ffmpegProcess = (0, child_process_1.spawn)('ffmpeg', options);
 ffmpegProcess.stdout.on('data', (data) => {
     console.log(`ffmpeg stdout: ${data}`);
 });
-// @ts-ignore
 ffmpegProcess.stderr.on('data', (data) => {
     console.error(`ffmpeg stderr: ${data}`);
 });
-// @ts-ignore
 ffmpegProcess.on('close', (code) => {
     console.log(`ffmpeg process exited with code ${code}`);
 });
-wss.on('connection', (ws) => {
-    console.log('Client connected to WebSocket');
-    // Assuming ffmpegProcess is properly initialized before this point
-    ws.on('message', (data) => {
-        if (data instanceof Buffer) {
-            // Handle binary data as a Buffer object
-            //console.log('Received binary data:', data);
-            // Check if ffmpegProcess and its stdin stream are initialized and writable
-            // @ts-ignore
-            if (ffmpegProcess && ffmpegProcess.stdin && ffmpegProcess.stdin.writable) {
-                // @ts-ignore
-                ffmpegProcess.stdin.write(data, (err) => {
-                    if (err) {
-                        console.error('Error writing to ffmpegProcess.stdin:', err);
-                    }
-                    else {
-                        console.log('Data written to ffmpegProcess.stdin successfully');
-                    }
-                });
-            }
-            else {
-                console.error('ffmpegProcess or its stdin stream is not properly initialized');
-            }
-        }
-        else if (typeof data === 'string') {
-            // Handle text data
-            console.log('Received text data:', data);
+io.on('connection', (socket) => {
+    console.log(`Client connected to Socket.IO`);
+    socket.on('webcam', (data) => {
+        if (ffmpegProcess && ffmpegProcess.stdin && ffmpegProcess.stdin.writable) {
+            ffmpegProcess.stdin.write(data, (err) => {
+                if (err) {
+                    console.error('Error writing to ffmpegProcess.stdin:', err);
+                }
+                else {
+                    console.log('Data written to ffmpegProcess.stdin successfully');
+                }
+            });
         }
         else {
-            // Handle other data types
-            console.log('Received data of unknown type:', data);
+            console.error('ffmpegProcess or its stdin stream is not properly initialized');
         }
     });
-    ws.on('close', () => {
-        console.log('Client disconnected from WebSocket');
+    socket.on('screen', (data) => {
+        if (ffmpegProcess && ffmpegProcess.stdin && ffmpegProcess.stdin.writable) {
+            ffmpegProcess.stdin.write(data, (err) => {
+                if (err) {
+                    console.error('Error writing to ffmpegProcess.stdin:', err);
+                }
+                else {
+                    console.log('Data written to ffmpegProcess.stdin successfully');
+                }
+            });
+        }
+        else {
+            console.error('ffmpegProcess or its stdin stream is not properly initialized');
+        }
     });
-    ws.on('error', (error) => {
-        console.error('WebSocket error:', error);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected from Socket.IO');
+    });
+    socket.on('error', (error) => {
+        console.error('Socket.IO error:', error);
     });
 });
 app.get('/', (req, res) => {
     res.send('Hello from server!');
 });
+// auth
+// app.use('/auth', login);
+// app.use('/auth', signup);
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,104 @@
-// Server side - Express app
 import express from 'express';
 import http from 'http';
-import WebSocket from 'ws';
-import { spawn } from 'child_process'
-import { options } from './rtmpOptions/options';
+import { spawn } from 'child_process';
+import { Server } from 'socket.io';
+// import login from './routes/auth/login';
+// import signup from './routes/auth/signup';
+
 const app = express();
 const port = 3000;
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
 
-// @ts-ignore
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"]
+  }
+});
+
+const options = [
+  '-i',
+  '-',
+  '-c:v', 'libx264',
+  '-preset', 'ultrafast',
+  '-tune', 'zerolatency',
+  '-r', '25',
+  '-g', '50', // This should be multiplied by 2 for keyframe interval (GOP size)
+  '-keyint_min', '25',
+  '-crf', '25',
+  '-pix_fmt', 'yuv420p',
+  '-sc_threshold', '0',
+  '-profile:v', 'main',
+  '-level', '3.1',
+  '-c:a', 'aac',
+  '-b:a', '128k',
+  '-ar', '32000', // Adjust the audio sample rate as needed
+  '-f', 'flv',
+  'rtmp://a.rtmp.youtube.com/live2/w0yv-1v24-4rp2-c3sa-3ze9',
+];
+
 const ffmpegProcess = spawn('ffmpeg', options);
 
-
-// @ts-ignore
 ffmpegProcess.stdout.on('data', (data) => {
   console.log(`ffmpeg stdout: ${data}`);
 });
 
-// @ts-ignore
 ffmpegProcess.stderr.on('data', (data) => {
   console.error(`ffmpeg stderr: ${data}`);
 });
 
-// @ts-ignore
 ffmpegProcess.on('close', (code) => {
   console.log(`ffmpeg process exited with code ${code}`);
 });
 
+io.on('connection', (socket) => {
+  console.log(`Client connected to Socket.IO`);
 
-wss.on('connection', (ws) => {
-  console.log('Client connected to WebSocket');
-
-  // Assuming ffmpegProcess is properly initialized before this point
-
-  ws.on('message', (data) => {
-    if (data instanceof Buffer) {
-      // Handle binary data as a Buffer object
-      //console.log('Received binary data:', data);
-
-      // Check if ffmpegProcess and its stdin stream are initialized and writable
-      // @ts-ignore
-      if (ffmpegProcess && ffmpegProcess.stdin && ffmpegProcess.stdin.writable) {
-        // @ts-ignore
-        ffmpegProcess.stdin.write(data, (err) => {
-          if (err) {
-            console.error('Error writing to ffmpegProcess.stdin:', err);
-          } else {
-            console.log('Data written to ffmpegProcess.stdin successfully');
-          }
-        });
-      } else {
-        console.error('ffmpegProcess or its stdin stream is not properly initialized');
-      }
-    } else if (typeof data === 'string') {
-      // Handle text data
-      console.log('Received text data:', data);
+  socket.on('webcam', (data) => {
+    if (ffmpegProcess && ffmpegProcess.stdin && ffmpegProcess.stdin.writable) {
+      ffmpegProcess.stdin.write(data, (err) => {
+        if (err) {
+          console.error('Error writing to ffmpegProcess.stdin:', err);
+        } else {
+          console.log('Data written to ffmpegProcess.stdin successfully');
+        }
+      });
     } else {
-      // Handle other data types
-      console.log('Received data of unknown type:', data);
+      console.error('ffmpegProcess or its stdin stream is not properly initialized');
     }
   });
 
-
-  ws.on('close', () => {
-    console.log('Client disconnected from WebSocket');
+  socket.on('screen', (data) => {
+    if (ffmpegProcess && ffmpegProcess.stdin && ffmpegProcess.stdin.writable) {
+      ffmpegProcess.stdin.write(data, (err) => {
+        if (err) {
+          console.error('Error writing to ffmpegProcess.stdin:', err);
+        } else {
+          console.log('Data written to ffmpegProcess.stdin successfully');
+        }
+      });
+    } else {
+      console.error('ffmpegProcess or its stdin stream is not properly initialized');
+    }
   });
 
-  ws.on('error', (error) => {
-    console.error('WebSocket error:', error);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected from Socket.IO');
+  });
+
+  socket.on('error', (error) => {
+    console.error('Socket.IO error:', error);
   });
 });
 
 app.get('/', (req, res) => {
   res.send('Hello from server!');
 });
+
+// auth
+// app.use('/auth', login);
+// app.use('/auth', signup);
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
